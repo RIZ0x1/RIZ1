923256c4de6d6e8d94e0d00d213a4a58dad14298
RIZ0x1 <svenPhilipsSven@mail.ru>
2020-03-31 03:28:25 +0000

    03.31 03:29

--- marathon00/error.c
+++ marathon00/error.c
@@ -1,7 +1,23 @@
 int	error(int argc, char argv[][], int done)
 {
-	if (argc != 10)
+	int i;
+	int j;
+
+	i = 0;
+	j = 0;
+	if	(argc != 10)
 		return (putError());
+	while	(i < 9)
+	{
+		while (j < 9)
+		{
+			if ((argv[i][j] > '9' || argv[i][j] < '0') && (argv[i][j] != '.'))
+				return (putError());
+			(j++);
+		}
+		j = 0;
+		(i++);
+	}
 	if (isDone(argv))
 	{
 		
