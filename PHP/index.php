0aff89df99e1107978695413c1dfe992c76a9dc7
RIZ0x1 <SvenPhilipsSven@mail.ru>
2020-05-07 07:25:55 +0000

    05.07 07:25

--- PHP/index.php
+++ PHP/index.php
@@ -1,3 +1,23 @@
 <?php
-echo 'Hello world!';
-?>
+	{ // 3
+		$text	= "The numbers: ";
+		$num	= 0x1f;
+		$num2	= -32;
+		$num3	= -0x07;
+		echo "{$text}$num | $num2 | $num3";
+	}
+	{ // 2
+		$str = <<< END
+		Здесь была твоя мамаша. Что она тут делала? Дрочус? Дракочус? Неее, она не такая
+		она больше любит анчоус!
+		END;
+
+		echo "{$str}!!!";
+	}
+	{ //3
+		if (isset($str))
+		{
+			unset($str);
+		}
+	}
+	{ //4
